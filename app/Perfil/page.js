2ec2169da862dc050ae9d0125b758f1c6e66b88c
cleon5 <cleon5@ucol.mx>
2023-07-29 20:14:10 -0600

    perfil update

--- app/Perfil/page.js
+++ app/Perfil/page.js
@@ -28,6 +28,15 @@ function Perfil() {
             "badge rounded-pill text-bg-dark",
         ],
     }
+    let num = 2
+    
+    const sliced = ()=>{
+      let x = data.skills.soft.slice((num - 1)*5,num*5)
+      console.log(x)
+    }
+      sliced()
+
+    
   return (
      <div className="Perfil">
         <div className="About">
@@ -71,17 +80,18 @@ function Perfil() {
             <div className="techStack">
                 <h3>Proyects</h3>
                 <hr/>
-                <div className="proyectsList">
+                <div className="">
                      {data.skills.soft.map((skill, index) => (
-                        <div className="card mb-3" style={{"width": "48%"}}>
+                        <div className="card mb-3" style={{"width": "95%"}}>
                         <div className="row g-0">
                           <div className="col-md-4">
-                            <img src={data.image} className="img-fluid rounded-start" alt="..."/>
+                            <img src={data.image} style={{"background-size": "cover", height:"100%"}} className="img-fluid rounded-start" alt="..."/>
                           </div>
                           <div className="col-md-8">
                             <div className="card-body">
-                              <h5 className="card-title">Card title</h5>
-                              <p className="card-text">Se creo una web en react para la </p>
+                              <h5 className="card-title">Pagina web para despliege de datps de una api</h5>
+                              <hr/>
+                              <p className="card-text">Se creo una web en react para la dsa asreo una web en react para la Se creo una web en react para la Se creo una web en react para la </p>
                               <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
                             </div>
                           </div>
@@ -96,25 +106,28 @@ function Perfil() {
             <div className="techStack">
                 <h3>Work</h3>
                 <hr/>
-                {data.skills.hard.map((skill, index) => (
-                        <div className="card mb-3" style={{"width": "95%"}}>
+                <div className="proyectsList">
+                  {data.skills.hard.map((skill, index) => (
+                        <div className="card mb-3" style={{"width": "48%", backgroundColor:""}}>
                         <div className="row g-0">
-                          <div className="col-md-4">
-                              <p className="card-text">Se creo una web en react para la </p>
-                              <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                          <div className="col-md-4 p-3">
+                              <p className="card-text text-end">Junio 2022 a Julio 2023 </p>
+                              <p className="card-text text-end"><small className="text-body-secondary">Cenedic</small></p>
                           </div>
                           <div className="col-md-8">
                             <div className="card-body">
-                              <h5 className="card-title">Programador jr</h5>
-                              <p className="card-text"><small className="">Last updated 3 mins ago</small></p>
+                              <h5 className="card-title">Programador jr </h5>
                               <hr />
-                              <p className="card-text">Se creo una web en react para la </p>
+                              <p className="card-text">Se creo una web en react para la gestion de tareas y el despliege de varios cursos
+                               </p>
                             </div>
                           </div>
                         </div>
                       </div>
                     ))
                 }
+                </div>
+                
             </div>
         </div>
 
